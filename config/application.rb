fd3829723436396872aa6820a45ae0ccd5c21584
estrada88 <estrada.ae@gmail.com>
2014-02-06 16:46:48 -0800

    add mongohq

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,15 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Meggings2
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
