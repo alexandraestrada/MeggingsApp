8e35f6041546cea3e4cc3955571f60bf9c45aee0
estrada88 <estrada.ae@gmail.com>
2014-02-11 11:13:02 -0800

    fixed bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   def create
 		user = User.create(params.require(:user).permit(:username, :password, :email))
 		session[:user_id] = user.id
-		redirect_to meggings_path
+		redirect_to users_path
 	end
 end
